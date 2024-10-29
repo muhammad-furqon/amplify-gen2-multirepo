c279af0a696e9a243d22c5c5ee6aa08193d948aa
muhammad-furqon <muhammad.furqon@cosmedia.co.jp>
2024-10-29 13:32:37 +0900

    Edit data schema auth mode

--- backend/amplify/data/resource.ts
+++ backend/amplify/data/resource.ts
@@ -19,7 +19,10 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+    defaultAuthorizationMode: "userPool",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
   },
 });
 
